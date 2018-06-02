817dfa1d396ed2741b24127e074bd5cbe94735b5
Amel Johny <amel.johny@gmail.com>
2018-06-02 15:39:19 +0530

    routing working

--- src/app/about/about-routing.module.ts
+++ src/app/about/about-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutPageComponent } from './about-page/about-page.component';
+import { AboutContentComponent } from './about-content/about-content.component';
+import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: AboutPageComponent
+    path: 'About',
+    component: AboutPageComponent,
+    children: [
+      {
+        path: '',
+        component:  AboutContentComponent,
+        pathMatch: 'full'
+      },
+      {
+        path: 'content',
+        component: AboutContentComponent
+      },
+      {
+        path: 'pageNotFound',
+        component: PageNotFoundComponent
+      }
+    ]
   }
 ];
 
